4ca61cfc3c8c358ca7b6882fbd8feffbaa6631dd
Ted Pyne <ted.pyne.4@gmail.com>
2018-04-03 19:02:38 -0400

    Add backend stuff

--- src/main/java/backend/Course.java
+++ src/main/java/backend/Course.java
@@ -1,5 +1,12 @@
 package backend;
 
-public class Course {
+import java.util.List;
+import java.util.Map;
 
+public class Course {		//Represents a course
+	private String name;
+	private boolean points;
+	private Map<String,Double> weights;
+	private List<Score> scores;
+	
 }
--- src/main/java/backend/Score.java
+++ src/main/java/backend/Score.java
@@ -1,5 +1,20 @@
 package backend;
 
 public class Score {
-
+	private String category, title,note;
+	private Double score;
+	public Score(String title,String note, String category,Double score){
+		this.category = category;
+		this.note = note;
+		this.title = title;
+		this.score = score;
+	}
+	
+	public String getCategory(){return category;}
+	
+	public String getTitle(){return title;}
+	
+	public String getNote(){return note;}
+	
+	public Double updateScore(){return score;}
 }
