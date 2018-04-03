fd4333cff55e991d6456c360c3cbdd8bbabb21b5
jere99 <jrdegreeff@gmail.com>
2018-04-03 16:19:14 -0400

    main class

---
+++ src/main/java/mxcs/gradeTracker/GradeTracker.java
@@ -0,0 +1,14 @@
+package mxcs.gradeTracker;
+
+/**
+ * The main class for the Grade Tracker.
+ * 
+ * @author Middlesex Computer Science Club 2018
+ */
+public class GradeTracker {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
