602461f35774bd57907748a8a495ecee94f8c0c5
Ted Pyne <ted.pyne.4@gmail.com>
2018-04-17 19:11:53 -0400

    Method headers for student

---
+++ src/main/java/mxcs/gradeTracker/backend/Student.java
@@ -0,0 +1,12 @@
+package mxcs.gradeTracker.backend;
+
+public class Student {
+	private String name;
+	public Student(String name){
+		this.name = name;
+	}
+	public addCourse(String name)
+	public getCourse(String name)
+	public removeCourse(String name)
+	public gradeSummary()
+}
