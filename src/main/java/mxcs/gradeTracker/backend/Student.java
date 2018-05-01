4e2105d5a9a2fbd2febff091349432d6be09ceeb
jere99 <jrdegreeff@gmail.com>
2018-05-01 18:45:42 -0400

    add course list

--- src/main/java/mxcs/gradeTracker/backend/Student.java
+++ src/main/java/mxcs/gradeTracker/backend/Student.java
@@ -1,20 +1,30 @@
 package mxcs.gradeTracker.backend;
 
+import java.util.List;
+
 public class Student {
+	
 	private String name;
+	private List<Course> courses;
+	
 	public Student(String name){
 		this.name = name;
 	}
+	
 	public void addCourse(String name) {
 		
 	}
+	
 	public void getCourse(String name) {
 		
 	}
+	
 	public void removeCourse(String name) {
 		
 	}
+	
 	public void gradeSummary() {
 		
 	}
+	
 }
